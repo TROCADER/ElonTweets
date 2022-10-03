7cb2098d5dff300593d58c01ae1954d1aef6376d
William Mårback <william.marback@hotmail.com>
2022-10-03 13:07:45 +0200

    Sums total of occurences for a word in a year.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import numpy
+import pandas
+
+dataFrame = pandas.read_csv("elonmusk.csv")
+
+userWord = input("Word: ")
+userYear = input("Year: ")
+
+occurences = 0
+for index, row in dataFrame.iterrows():
+    if userYear in row["Date Created"] and userWord in row["Tweets"]:
+        occurences += 1
+
+print(occurences)
