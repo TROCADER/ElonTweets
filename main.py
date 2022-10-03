60d00ee9972b158e7dbcc889d6a4fec8a3282d82
William Mårback <william.marback@hotmail.com>
2022-10-03 13:53:46 +0200

    Adds occurences to dict keyed with year.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import U
 import numpy
 import pandas
 
@@ -6,10 +7,21 @@ dataFrame = pandas.read_csv("elonmusk.csv")
 userWord = input("Word: ")
 userYear = input("Year: ")
 
+#startYear = input("Start year: ")
+#endYear = input("End year: ")
+#currentYear = startYear
+
+occurencesDict = {}
+
 occurences = 0
 for index, row in dataFrame.iterrows():
     if userYear in row["Date Created"] and userWord in row["Tweets"]:
-        occurences += 1
+        #occurences += 1
 
+        if userYear in occurencesDict:
+            occurencesDict[userYear] += 1
+        else:
+            occurencesDict[userYear] = 1
 
-print(occurences)
+for year in occurencesDict:
+    print(f"{year}: {occurencesDict[year]}")
