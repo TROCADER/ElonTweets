60e4534739e1b48fe8b441d68352dd5eb114d461
William Mårback <william.marback@hotmail.com>
2022-10-03 14:47:43 +0200

    Code cleanup

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from re import U
-import numpy
 import pandas
 
 dataFrame = pandas.read_csv("elonmusk.csv")
@@ -7,17 +5,10 @@ dataFrame = pandas.read_csv("elonmusk.csv")
 userWord = input("Word: ")
 userYear = input("Year: ")
 
-#startYear = input("Start year: ")
-#endYear = input("End year: ")
-#currentYear = startYear
-
 occurencesDict = {}
 
-occurences = 0
 for index, row in dataFrame.iterrows():
     if userYear in row["Date Created"] and userWord in row["Tweets"]:
-        #occurences += 1
-
         if userYear in occurencesDict:
             occurencesDict[userYear] += 1
         else:
