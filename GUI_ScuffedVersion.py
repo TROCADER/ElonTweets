1756bd310c70e6543b208c389ffcca065ab570a7
William Mårback <william.marback@hotmail.com>
2022-10-17 15:01:14 +0200

    Added startyear

--- GUI_ScuffedVersion.py
+++ GUI_ScuffedVersion.py
@@ -22,7 +22,7 @@ toolbar = NavigationToolbar2Tk(canvas, root)
 def draw_chart():
     fig.clear()
     userWord = wordEntry.get()
-    currentYear = int(dateEndEntry.get())
+    currentYear = int(yearEndEntry.get())
     output = {currentYear:0}
     head = userWord[0]
     if len(userWord) > 1:
@@ -30,23 +30,24 @@ def draw_chart():
         for i in range(1,len(userWord)):
             tail += userWord[i]
 
-    for index, row in elonsTweets.iterrows():  
-        if caseSensitive.get() == True:
-            if currentYear == int(row["Date Created"][:4]):
-                #Funkar inte med userWord eller wordEntry.get(), kan inte kontrollera med value från textrutan
-                #Koden är densamma som i gamla versionen, den gamla kodfilen utan GUI
-                output[currentYear] += len(re.findall(rf"\b({userWord}\b", row["Tweets"])) 
-            elif currentYear > int(row["Date Created"][:4]):
-                currentYear -= 1
-                output[currentYear] = 0
-        else:
-            if currentYear == int(row["Date Created"][:4]):
-                #Funkar inte som den skall, t.ex. returnerar ett ord som slutar på en stor bokstav 0 resultat
-                #Funkar enbart om ordet börjar på stor bokstav
-                output[currentYear] += len(re.findall(rf"\b({head.upper()}|{head}){tail}\b", row["Tweets"]))
-            elif currentYear > int(row["Date Created"][:4]):
-                currentYear -= 1
-                output[currentYear] = 0
+    for index, row in elonsTweets.iterrows():
+        if currentYear >= int(yearStartEntry.get()):
+            if caseSensitive.get() == True:
+                if currentYear == int(row["Date Created"][:4]):
+                    #Funkar inte med userWord eller wordEntry.get(), kan inte kontrollera med value från textrutan
+                    #Koden är densamma som i gamla versionen, den gamla kodfilen utan GUI
+                    output[currentYear] += len(re.findall(rf"\b({userWord}\b", row["Tweets"])) 
+                elif currentYear > int(row["Date Created"][:4]):
+                    currentYear -= 1
+                    output[currentYear] = 0
+            else:
+                if currentYear == int(row["Date Created"][:4]):
+                    #Funkar inte som den skall, t.ex. returnerar ett ord som slutar på en stor bokstav 0 resultat
+                    #Funkar enbart om ordet börjar på stor bokstav
+                    output[currentYear] += len(re.findall(rf"\b({head.upper()}|{head}){tail}\b", row["Tweets"]))
+                elif currentYear > int(row["Date Created"][:4]):
+                    currentYear -= 1
+                    output[currentYear] = 0
     
     fig.add_subplot(111).plot(list(output.keys()), list(output.values()))
     canvas.draw_idle()
@@ -57,16 +58,16 @@ def draw_chart():
     csvReadyDF.to_csv("output.csv", index=False)
 
 wordEntry = Entry(root, width=5, font=("calibre", 10, "normal"))
-dateStartEntry = Entry(root, width=5, font=("calibre", 10, "normal"))
-dateEndEntry = Entry(root, width=5, font=("calibre", 10, "normal"))
+yearStartEntry = Entry(root, width=5, font=("calibre", 10, "normal"))
+yearEndEntry = Entry(root, width=5, font=("calibre", 10, "normal"))
 caseSensitive = BooleanVar()
 caseSensitiveEntry = Checkbutton(root, variable=caseSensitive, onvalue=True, offvalue=False)
 Label(root, text="Enter word").pack()
 wordEntry.pack()
 Label(root, text="Enter start date").pack()
-dateStartEntry.pack()
+yearStartEntry.pack()
 Label(root, text="Enter end date").pack()
-dateEndEntry.pack()
+yearEndEntry.pack()
 Label(root, text="Case sensitive?", width=20).pack()
 caseSensitiveEntry.pack()
 Button(root,text="Draw", command=draw_chart).pack()
