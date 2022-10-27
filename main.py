789839ae147c93e046a303106ee8f9634d3bb9c2
William Mårback <william.marback@hotmail.com>
2022-10-27 16:31:10 +0200

    Can plot existing csv.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Importerar alla nödvändiga bibliotek som används i projektet
+from cProfile import label
 from tkinter import *
 #from traceback import print_tb
 import pandas as pd
@@ -26,22 +27,33 @@ def draw_chart():
     # Rensar rutan så att enbart ny data syns
     fig.clear()
     # Skapar en dict som datan från förekomsterna sparas i
-    output = {int(yearEndEntry.get()):0}
+    output = {None:None}
 
-    output = findOccurencesInpdDF(int(yearEndEntry.get()), int(yearStartEntry.get()), wordEntry.get(), output, caseSensitive.get(), pd.read_csv("elonmusk.csv"))
+    # Ger användaren möjligheten att kunna läsa in filer som redan finns på systemet, användbart om man exempelvis vill kolla på den senaste datan igen
+    if onlyPrint.get() == True:
+        readCsv = pd.read_csv("data.csv")
+        fig.add_subplot(111).plot(readCsv["Year"], readCsv["Occurences"])
+
+    # Om användaren inte vill kolla på redan existerande data så genereras ny som skriver över det som redan finns (om det finns något)
+    else:
+        output = {int(yearEndEntry.get()):0}
+        output = findOccurencesInpdDF(int(yearEndEntry.get()), int(yearStartEntry.get()), wordEntry.get(), output, caseSensitive.get(), pd.read_csv("elonmusk.csv"))
+
+        # Skapar en pandas dataframe av dict:en där alla förekomster lagrades
+        # Eftersom den nyligen skapta DF:en är felaktigt formaterad för att kunna användas väl måste dess index nollställas samt columner läggas till
+        csvReadyDF = pd.DataFrame.from_dict(output, orient="index").reset_index()
+        print(csvReadyDF)
+        csvReadyDF.columns = ["Year", "Occurences"]
+
+        # Skriver DF:en med all data till en .csv som kan användas i andra projekt, samt för att se antalet förekomster i mer detalj
+        csvReadyDF.to_csv("data.csv", index=False)
+
+        # Lägger till en graf genererad av matplotlib enligt formatet, 1 rad, 1 column på graf 1 (111)
+        fig.add_subplot(111).plot(list(output.keys()), list(output.values()))
     
-    # Lägger till en graf genererad av matplotlib enligt formatet, 1 rad, 1 column på graf 1 (111)
-    fig.add_subplot(111).plot(list(output.keys()), list(output.values()))
     # Ritar ut figuren 1 gång
     canvas.draw_idle()
 
-    # Skapar en pandas dataframe av dict:en där alla förekomster lagrades
-    # Eftersom den nyligen skapta DF:en är felaktigt formaterad för att kunna användas väl måste dess index nollställas samt columner läggas till
-    csvReadyDF = pd.DataFrame.from_dict(output, orient="index").reset_index()
-    csvReadyDF.columns = ["Year", "Occurences"]
-
-    # Skriver DF:en med all data till en .csv som kan användas i andra projekt, samt för att se antalet förekomster i mer detalj
-    csvReadyDF.to_csv("output.csv", index=False)
 
 def findOccurencesInpdDF(activeYear, yearStart, wordToCheck, outputDict, sensitiveOrNot, pdDFToCheck):
     for index, row in pdDFToCheck.iterrows():
@@ -73,9 +85,11 @@ wordEntry = Entry(root, width=5, font=("calibre", 10, "normal"))
 yearStartEntry = Entry(root, width=5, font=("calibre", 10, "normal"))
 yearEndEntry = Entry(root, width=5, font=("calibre", 10, "normal"))
 # Gör en checkbox som ändrar variabeln nedan, variabeln används i huvudlogiken
-# Eftersom att man inte direkt kan hämta datan från en checkbox så behöver vi först ändra på en variabel till checkboxens data, därmed varför en ytterliggare variabel finns
+# Eftersom att man inte direkt kan hämta datan från en checkbox så behöver vi först ändra på en variabel till checkboxens data, därmed varför ytterliggare variablar finns
 caseSensitive = BooleanVar()
 caseSensitiveEntry = Checkbutton(root, variable=caseSensitive, onvalue=True, offvalue=False)
+onlyPrint = BooleanVar()
+onlyPrintEntry = Checkbutton(root, variable=onlyPrint, onvalue=True, offvalue=False)
 # Label för en "lapp" som säger något, varav i detta fall frågar användaren
 # .pack() gör datan redo att presenteras i GUI:t, gör så att tkinter vet hur den ska skriva ut den
 Label(root, text="Enter word").pack()
@@ -86,8 +100,11 @@ Label(root, text="Enter end year").pack()
 yearEndEntry.pack()
 Label(root, text="Case sensitive?", width=20).pack()
 caseSensitiveEntry.pack()
+Label(root, text="Read from existing .csv?", width=20).pack()
+onlyPrintEntry.pack()
 # Knapp som startar logiken
 Button(root,text="Draw", command=draw_chart).pack()
 
+# Håller rutan igång och därmed programmet igång
+# Tkinter kör om sålänge som rutan är öppen, därmed varför en while-loop in behövs/kan användas
 root.mainloop()
-
